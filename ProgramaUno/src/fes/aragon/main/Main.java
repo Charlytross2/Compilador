491ca3810c312b327f22cd76d53f5ebf4e02d806
Charlytross2 <carlosgonzalez24@aragon.unam.mx>
2024-03-10 12:22:36 -0600

    Finished

--- ProgramaUno/src/fes/aragon/main/Main.java
+++ ProgramaUno/src/fes/aragon/main/Main.java
@@ -3,9 +3,9 @@ package fes.aragon.main;
 import fes.aragon.files.FontFileReader;
 import fes.aragon.program.Core;
 
-import java.awt.*;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 public class Main {
@@ -13,15 +13,16 @@ public class Main {
         try {
             List<String> sources = FontFileReader.getSourceFromFile();
             Core core = new Core(sources);
-            List<Boolean> results = core.resolveSources();
+            List<Map<String, Boolean>> results = core.resolveSources();
 
             IntStream.range(0, results.size()).forEach(index -> {
-                System.out.print(sources.get(index));
-                if (results.get(index)) {
-                    System.out.print(" -> Valid\n");
-                } else {
-                    System.out.print(" -> Invalid\n");
-                }
+                Object[] keys = results.get(index).keySet().toArray();
+                Object[] values = results.get(index).values().toArray();
+
+                System.out.println(sources.get(index) + ":");
+                System.out.println(keys[0] + "->" + values[0]);
+                System.out.println(keys[1] + "->" + values[1]);
+                System.out.println();
             });
         } catch (Exception e) {
             System.out.println(e.getMessage());
--- ProgramaUno/src/fes/aragon/program/Core.java
+++ ProgramaUno/src/fes/aragon/program/Core.java
@@ -1,13 +1,14 @@
 package fes.aragon.program;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Core {
 
     private List<String> sources;
-    private List<Boolean> responses;
+    private List<Map<String,Boolean>> responses;
 
 
     public Core(List<String> sources) {
@@ -15,43 +16,73 @@ public class Core {
         this.responses = new ArrayList<>();
     }
 
-    public List<Boolean> resolveSources() throws Exception {
-        if(this.sources.isEmpty()){
+    public List<Map<String,Boolean>> resolveSources() throws Exception {
+        if (this.sources.isEmpty()) {
             throw new Exception();
         }
 
-        this.sources.stream().forEach(source -> {
+        this.sources.forEach(source -> {
             // initial state
-            int state = 0;
+            int statePairs = 0;
+            int stateOdds = 0;
             int errorCol = -1;
             boolean error = false;
 
             for (int i = 0; i <= source.length() - 1; i++) {
                 char c = source.charAt(i);
 
-                if(c != '0' && c != '1') {
+                if (c != '0' && c != '1') {
                     error = true;
                     errorCol = i;
                     break;
-                };
+                }
 
-                switch (state) {
+                switch (statePairs) {
                     case 0:
-                        if (c == '1') state = 0;
-                        else if (c == '0') state = 1;
+                        if (c == '1') statePairs = 1;
+                        else if (c == '0') statePairs = 0;
                         break;
                     case 1:
-                        if (c == '1') state = 1;
-                        else if (c == '0') state = 0;
+                        if (c == '1') statePairs = 0;
+                        else if (c == '0') statePairs = 1;
+                        break;
+                }
+
+                switch (stateOdds){
+                    case 0:
+                        if (c == '1') stateOdds = 0;
+                        else if (c == '0') stateOdds = 1;
+                        break;
+                    case 1:
+                        if (c == '1') stateOdds = 1;
+                        else if (c == '0') stateOdds = 0;
                         break;
                 }
             }
 
-            if (state == 1 && !error) this.responses.add(true);
-            else {
-                this.responses.add(false);
-                if(error && errorCol >= 0) System.out.printf("Invalid character at column %d",errorCol);
-            };
+            HashMap<String,Boolean> map = new HashMap<>();
+
+            if (statePairs == 0 && !error) {
+                map.put("1 pairsTrue", true);
+            } else if (statePairs != 0 || error) {
+                map.put("1 pairsFalse", false);
+                if (error && errorCol >= 0) {
+                    System.out.printf("Invalid character at column %d", errorCol);
+                }
+            }
+
+            if (stateOdds == 1 && !error) {
+                map.put("0 oddsTrue", true);
+            } else if (stateOdds != 1 || error) {
+                map.put("0 oddsFalse", false);
+                if (error && errorCol >= 0) {
+                    System.out.printf("Invalid character at column %d", errorCol);
+                }
+            }
+
+
+            this.responses.add(map);
+
         });
 
         return this.responses;
